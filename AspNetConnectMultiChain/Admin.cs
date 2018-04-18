dd9dd5f0197c1864cb1aeac9996b16551ad17d2e
baonv <baonv@ilandapp.com>
2018-04-18 14:30:40 +0700

    init project

---
+++ AspNetConnectMultiChain/Admin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class.MultichainLib
+{
+    public class Admin: ServiceBase
+    {
+
+        public string GetInfo()
+        {
+            return jsonRpcClient.JsonRpcRequest("getinfo");
+        }
+
+        public Admin(JsonRpcClient c):base(c)
+        {
+
+        }
+    }
+}
---
+++ ConnectMultiChain/Controllers/MultiChainController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Class.MultichainLib;
+
+namespace ConnectMultiChain.Controllers
+{
+    public class MultiChainController : Controller
+    {
+        // GET: MultiChain
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string GetInfo(string rpcPassword, string ipAddress, int port, string chainName)
+        {
+            try
+            {
+                JsonRpcClient jsonRpcClient = new JsonRpcClient("multichainrpc", rpcPassword, ipAddress, port, chainName);
+                Admin admin = new Admin(jsonRpcClient);
+                string json = "";
+                json = admin.GetInfo();
+                return json;
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+    }
+}
